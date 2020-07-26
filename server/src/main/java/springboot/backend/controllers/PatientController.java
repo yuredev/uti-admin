281ded3d94ca7980e5c4e520dbf976c48e2b8407
Yure Matias <yurematias@ufrn.edu.br>
2020-07-26 19:44:33 -0300

    unusual imports removed

--- server/src/main/java/springboot/backend/controllers/PatientController.java
+++ server/src/main/java/springboot/backend/controllers/PatientController.java
@@ -1,37 +1,31 @@
 package springboot.backend.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springboot.backend.models.Medicine;
 import springboot.backend.models.Patient;
-import springboot.backend.services.MedicineService;
 import springboot.backend.services.PatientService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
 @CrossOrigin(origins = {"http://localhost:3000"}, exposedHeaders = "X-Total-Count")
 public class PatientController {
-    private final PatientService service;
-    private final MedicineService medService;
+    private final PatientService patientService;
 
-    public PatientController(PatientService service, MedicineService medService) {
-        this.service = service;
-        this.medService = medService;
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
     }
 
     @GetMapping
     public List<Patient> list() {
-        return service.getAll();
+        return patientService.getAll();
     }
 
     @GetMapping(path = {"/{id}"})
     public ResponseEntity<Patient> getByCpf(@PathVariable Integer id) {
-        Patient patientFound = service.getOne(id);
+        Patient patientFound = patientService.getOne(id);
         if (patientFound == null) {
             return ResponseEntity.notFound().build();
         }
@@ -40,30 +34,30 @@ public class PatientController {
 
     @PostMapping
     public ResponseEntity<Patient> insert(@RequestBody Patient patient) {
-        if (service.didAlreadyExists(patient.getCpf())) {
+        if (patientService.didAlreadyExists(patient.getCpf())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
-        Patient patientSaved = service.save(patient);
+        Patient patientSaved = patientService.save(patient);
         return ResponseEntity.status(HttpStatus.CREATED).body(patientSaved);
     }
 
     @PutMapping(path = "/{id}")
     public ResponseEntity<Patient> update(@PathVariable Integer id, @RequestBody Patient patient) {
         // if anyone patient owns the id of the path variable
-        if (!service.didAlreadyExists(id)) {
+        if (!patientService.didAlreadyExists(id)) {
             return ResponseEntity.notFound().build();
         }
-        Patient patientSaved = service.save(patient);
+        Patient patientSaved = patientService.save(patient);
         return ResponseEntity.ok().body(patientSaved);
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
-        Patient patientFound = service.getOne(id);
+        Patient patientFound = patientService.getOne(id);
         if (patientFound == null) {
             return ResponseEntity.notFound().build();
         }
-        service.delete(patientFound);
+        patientService.delete(patientFound);
         return ResponseEntity.ok().build();
     }
 }
